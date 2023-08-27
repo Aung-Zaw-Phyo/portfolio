3a3a8144f783c487b78f332015a7b8929c6b95d1
aungzawphyo <aungzawphyo1102@gmail.com>
2023-08-27 18:15:55 +0630

    add meta and favicon

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="container py-6 h-screen w-screen ">
             <div className="h-full w-full flex flex-col justify-center items-center p-4 ">
                 <img className="w-[200px] h-[200px] rounded-full mx-auto mb-6" src={me2} alt="" />
-                <h1 className="text-[24px] sm:text-[32px] mb-4 inline-flex ">
+                <h1 className="text-[24px] sm:text-[30px] mb-4 inline-flex ">
                     <div className="static-txt">Hi, I am </div>
                     <ul className="dynamic-txts m-0 p-0">
                         <li><span>Web Developer</span></li>
